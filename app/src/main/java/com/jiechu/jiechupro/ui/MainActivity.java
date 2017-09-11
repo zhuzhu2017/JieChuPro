b526ef98641d35b40499348ed7b626a0cf8f161b
小狄V5 <15038098307@163.com>
2017-09-11 17:28:28 +0800

    新建资源文件

--- app/src/main/java/com/jiechu/jiechupro/ui/MainActivity.java
+++ app/src/main/java/com/jiechu/jiechupro/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.jiechu.jiechupro;
+package com.jiechu.jiechupro.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jiechu.jiechupro.R;
 
 public class MainActivity extends AppCompatActivity {
 
